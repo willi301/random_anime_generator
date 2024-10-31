05dfac7b3b12f6034ec4c3a5ba842d32efce4942
willi301 <will301will@gmail.comm>
2024-10-31 11:31:36 -0700

    add button

--- random_anime_generator/src/App.js
+++ random_anime_generator/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Testing hi</h1>
+        <h1>Random Anime generator</h1>
+
+
+        <button>Generate</button>
       </header>
     </div>
   );
